2941cbb0f7cb218919f9ed34d344b6c038cfd773
Caio Almeida <caioalmeidanaweb@gmail.com>
2020-01-04 09:30:16 -0400

    finalização do projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {StatusBar} from 'react-native';
+import './config/ReactotronConfig';
+import {store, persistor} from './store';
+import AuthRoutes from './AuthRoutes';
+
+// import { Container } from './styles';
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <AuthRoutes />
+      </PersistGate>
+    </Provider>
+  );
+}
